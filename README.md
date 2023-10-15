865088c0b32804f3fa8e7473214cacd303f342e5
reyura <reyura.a@gmail.com>
2023-10-15 23:48:54 +0800

    🎉 initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# medusa-file-gcs
+
+## Features
+
+- Store product images on Google Cloud Storage.
+- Support for importing and exporting data through CSV files, such as Products or Prices.
+- Support for both private and public buckets.
+
+
+## How to Install
+
+1\. Run the following command in the directory of the Medusa backend:
+
+  ```bash
+  npm install @reyura/medusa-file-gcs
+  ```
+
+2\. Set the following environment variables in `.env`:
+
+  ```bash
+  GCS_PROJECT_ID=<GCS_PROJECT_ID>
+  GCS_BUCKET=<GCS_BUCKET>
+  GCS_PRIVATE_BUCKET=<GCS_PRIVATE_BUCKET>
+  GCS_EMAIL=<GCS_EMAIL>
+  GCS_PRIVATE_KEY=<GCS_PRIVATE_KEY>
+  ```
+
+3\. In `medusa-config.js` add the following at the end of the `plugins` array:
+
+  ```js
+  const plugins = [
+    // ...
+    {
+      resolve: `@reyura/medusa-file-gcs`,
+      options: {
+        projectId: process.env.GCS_PROJECT_ID,
+        bucketName: process.env.GCS_BUCKET,
+        privatebucketName: process.env.GCS_PRIVATE_BUCKET,
+        email: process.env.GCS_EMAIL,
+        privateKey: process.env.GCS_PRIVATE_KEY,
+        privateEmail: process.env.GCS_EMAIL,
+        privatePrivateKey: process.env.GCS_PRIVATE_KEY,
+      },
+    },
+  ]
+  ```
---
+++ src/services/gcs.ts
@@ -0,0 +1,174 @@
+import { AbstractFileService, IFileService } from "@medusajs/medusa";
+import { Storage } from "@google-cloud/storage";
+import {
+  DeleteFileType,
+  FileServiceUploadResult,
+  GetUploadedFileType,
+  UploadStreamDescriptorType,
+} from "@medusajs/types";
+import { MedusaError } from "medusa-core-utils";
+
+import stream from "stream";
+import { GetSignedUrlConfig } from "@google-cloud/storage";
+import { parse } from "path";
+
+export interface CredentialBody {
+  client_email?: string;
+  private_key?: string;
+}
+
+export interface GcpStorageServiceOptions {
+  credentials: CredentialBody;
+  bucket: string;
+}
+
+type File = Express.Multer.File;
+
+class GcpStorageService extends AbstractFileService implements IFileService {
+  protected projectId: string;
+  protected bucketName: string;
+  protected privatebucketName: string;
+  protected email: string;
+  protected privateKey: string;
+  protected privateEmail: string;
+  protected privatePrivateKey: string;
+
+  constructor({}, options) {
+    super({}, options);
+    this.projectId = options.projectId;
+    this.bucketName = options.bucketName;
+    this.privatebucketName = options.privatebucketName;
+    this.email = options.email;
+    this.privateKey = options.privateKey;
+    this.privateEmail = options.privateEmail;
+    this.privatePrivateKey = options.privatePrivateKey;
+  }
+
+  protected getBaseUrl(bucketName: string) {
+    return `https://storage.googleapis.com/${bucketName}/`;
+  }
+
+  protected getClient(usePrivateBucket = false) {
+    return new Storage({
+      projectId: this.projectId,
+      credentials: {
+        client_email: usePrivateBucket ? this.email : this.privateEmail,
+        private_key: usePrivateBucket ? this.privateKey : this.privatePrivateKey,
+      },
+    });
+  }
+  protected getBucket(usePrivateBucket = false) {
+    const storage = this.getClient(usePrivateBucket);
+    return storage.bucket(usePrivateBucket ? this.privatebucketName : this.bucketName);
+  }
+
+  validatePrivateBucketConfiguration_(usePrivateBucket: boolean) {
+    if (
+      usePrivateBucket &&
+      (!this.privateEmail || !this.privatePrivateKey || !this.privatebucketName)
+    ) {
+      throw new MedusaError(MedusaError.Types.UNEXPECTED_STATE, "Private bucket is not configured");
+    }
+  }
+
+  async upload(file: File): Promise<FileServiceUploadResult> {
+    return await this.uploadFile(file);
+  }
+
+  async uploadProtected(file: File): Promise<FileServiceUploadResult> {
+    this.validatePrivateBucketConfiguration_(true);
+
+    return await this.uploadFile(file, { isProtected: true });
+  }
+
+  protected async uploadFile(
+    file: File,
+    options: { isProtected: boolean } = { isProtected: false }
+  ) {
+    const parsedFilename = parse(file.originalname);
+    const fileKey = `${parsedFilename.name}-${Date.now()}${parsedFilename.ext}`;
+
+    const bucket = this.getBucket(options.isProtected);
+
+    const result = await bucket.upload(file.path, {
+      predefinedAcl: options.isProtected ? "private" : "publicRead",
+      destination: fileKey,
+    });
+
+    return { url: result[0].publicUrl(), key: result[0].kmsKeyName ?? "" };
+  }
+
+  async delete(file: DeleteFileType): Promise<void> {
+    const bucket = this.getBucket(false);
+    const privateBucket = this.getBucket(true);
+
+    await Promise.all([
+      new Promise((resolve, reject) =>
+        bucket.file(file.fileKey).delete((err, apiResponse) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(apiResponse);
+        })
+      ),
+      new Promise((resolve, reject) =>
+        privateBucket.file(file.fileKey).delete((err, apiResponse) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(apiResponse);
+        })
+      ),
+    ]);
+  }
+
+  async getUploadStreamDescriptor(
+    fileData: UploadStreamDescriptorType & {
+      contentType?: string;
+    }
+  ) {
+    const usePrivateBucket = fileData.isPrivate ?? true;
+
+    this.validatePrivateBucketConfiguration_(usePrivateBucket);
+
+    const bucket = this.getBucket(usePrivateBucket);
+
+    const fileKey = `${fileData.name}.${fileData.ext}`;
+    const pass = new stream.PassThrough();
+
+    return {
+      writeStream: pass,
+      promise: new Promise(() => pass.pipe(bucket.file(fileKey).createWriteStream())),
+      url: `${this.getBaseUrl(bucket.name)}/${fileKey}`,
+      fileKey,
+    };
+  }
+
+  async getDownloadStream(fileData: GetUploadedFileType) {
+    const usePrivateBucket = fileData.isPrivate ?? true;
+    this.validatePrivateBucketConfiguration_(usePrivateBucket);
+    const bucket = this.getBucket(usePrivateBucket);
+
+    return bucket.file(fileData.fileKey).createReadStream();
+  }
+
+  async getPresignedDownloadUrl({ isPrivate = true, ...fileData }: GetUploadedFileType) {
+    this.validatePrivateBucketConfiguration_(isPrivate);
+
+    const bucket = this.getBucket(isPrivate);
+    const fileKey = fileData.fileKey;
+
+    const options = {
+      version: "v4",
+      action: "read",
+      expires: Date.now() + 15 * 60 * 1000, // 15 MINUTES
+    } as GetSignedUrlConfig;
+
+    const ret = await bucket.file(fileKey).getSignedUrl(options);
+    return ret[0];
+  }
+}
+
+export default GcpStorageService;
